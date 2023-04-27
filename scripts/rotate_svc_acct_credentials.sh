71f9fb3688cf64cf06c304984125b2acf7ff1149
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-04-26 23:34:44 -0500

    [nc] add script for creating/rotating service account credentials
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ scripts/rotate_svc_acct_credentials.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+export ENVIRONMENT=$1
+export AWS_ACCOUNT_ID=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_account_id)
+
+aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/PSKIamProfilesRole --role-session-name psk-aws-iam-profiles > credentials
+
+export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
+export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
+export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
+export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
+
+# create/rotate service account credentials
+iam-credential-rotation PSKServiceAccounts > machine_credentials.json
+
+# write PSKNonprodServiceAccount credentials
+PSKNonprodServiceAccountCredentials=$(cat machine_credentials.json | jq .PSKNonprodServiceAccount)
+echo $PSKNonprodServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | opw write aws-dps-2 PSKNonprodServiceAccount-aws-access-key-id -
+echo $PSKNonprodServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | opw write aws-dps-2 PSKNonprodServiceAccount-aws-secret-access-key -
+
+# write PSKProdrodServiceAccount credentials
+PSKProdServiceAccountCredentials=$(cat machine_credentials.json | jq .PSKProdServiceAccount)
+echo $PSKProdServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | opw write aws-dps-2 PSKProdServiceAccount-aws-access-key-id -
+echo $PSKProdServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | opw write aws-dps-2 PSKProdServiceAccount-aws-secret-access-key -
