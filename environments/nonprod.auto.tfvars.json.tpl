e822ab5cb05604802179ec449445bdf70a2a6d98
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-04-26 22:22:25 -0500

    [nc] allow necessary permissions
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- environments/nonprod.auto.tfvars.json.tpl
+++ environments/nonprod.auto.tfvars.json.tpl
@@ -3,6 +3,7 @@
   "aws_account_id": "{{ op://empc-lab/aws-dps-2/aws-account-id }}",
 
   "is_state_account": true,
+  "state_account_id": "{{ op://empc-lab/aws-dps-2/aws-account-id }}",
   "all_nonprod_account_roles": [
       "arn:aws:iam::{{ op://empc-lab/aws-dps-2/aws-account-id }}:role/*"
   ],
--- variables.tf
+++ variables.tf
@@ -19,6 +19,11 @@ variable "is_state_account" {
   default     = false
 }
 
+variable "state_account_id" {
+  description = "arn principal root reference to state account id where all svc accounts are defined"
+  type        = string
+}
+
 variable "all_nonprod_account_roles" {
   description = "arn reference to * roles for all non-production aws accounts; arn:aws:iam::*****12345:role/*"
   type        = list(any)
@@ -28,62 +33,3 @@ variable "all_production_account_roles" {
   description = "arn reference to * roles for all production aws accounts; arn:aws:iam::*****12345:role/*"
   type        = list(any)
 }
-
-
-
-# # ========= original
-
-# # is the plan/apply running against the profiles account?
-# variable "is_state_account" {
-#   type     = bool
-#   default  = false
-# }
-
-# variable "aws_region" {}
-# variable "assume_role" {}
-
-# variable "account_id" {
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "prod_account_id" {
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "nonprod_account_id" {
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "datadog_api_key" {
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "datadog_app_key" {
-#   type        = string
-#   sensitive   = true
-# }
-
-
-# # twdps.io@gmail.com service account gpg public key for encrypting aws credentials
-# # not a secret, but even public keys can set off secret scanners
-# variable "twdpsio_gpg_public_key_base64" {
-#   type        = string
-#   sensitive   = true
-# }
-
-# # ========= new
-
-# variable "is_prod_account" {}
-# variable "aws_default_region" {}
-# variable "aws_account_role" {}
-
-# variable "aws_account_id" {
-#   type        = string
-#   sensitive   = true
-# }
-# variable "all_nonprod_account_roles" {}
-# variable "all_production_account_roles" {}
