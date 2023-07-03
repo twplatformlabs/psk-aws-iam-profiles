bae9721ff8c29ffe9c6c3c6836ac50e1cbf9b278
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-07-02 20:30:38 -0500

    [nc] terraform 1.5
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
   # pin major.minor versions
-  required_version = "~> 1.4"
+  required_version = "~> 1.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.61"
+      version = "~> 5.6"
     }
   }
 
