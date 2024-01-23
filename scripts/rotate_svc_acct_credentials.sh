9e69ccce6ce284b06e69405539f3903f57546cda
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-01-23 13:57:12 -0600

    [nc] continue new terraaform orb testing, updates to write-credential script
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/rotate_svc_acct_credentials.sh
+++ scripts/rotate_svc_acct_credentials.sh
@@ -10,15 +10,24 @@ export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAcces
 export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
 export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
 
+# Rotate AWS IAM User access credentials. https://pypi.org/project/iam-credential-rotation/
 echo "rotate service account credentials"
 iam-credential-rotation PSKServiceAccounts > machine_credentials.json
 
+# Write new nonprod credentials to 1password
 echo "write PSKNonprodServiceAccount credentials"
 PSKNonprodServiceAccountCredentials=$(cat machine_credentials.json | jq .PSKNonprodServiceAccount)
-op item edit 'aws-dps-2' PSKNonprodServiceAccount-aws-access-key-id=$(echo $PSKNonprodServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | tr -d \\n) >/dev/null
-op item edit 'aws-dps-2' PSKNonprodServiceAccount-aws-secret-access-key=$(echo $PSKNonprodServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | tr -d \\n) >/dev/null
+PSKNonprodAccessKey=$(echo $PSKNonprodServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | tr -d \\n)
+PSKNonprodSecret=$(echo $PSKNonprodServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | tr -d \\n)
 
+op item edit 'aws-dps-2' PSKNonprodServiceAccount-aws-access-key-id=$PSKNonprodAccessKey --vault empc-lab >/dev/null
+op item edit 'aws-dps-2' PSKNonprodServiceAccount-aws-secret-access-key=$PSKNonprodSecret --vault empc-lab >/dev/null
+
+# Write new prod credentials to 1password
 echo "write PSKProdrodServiceAccount credentials"
 PSKProdServiceAccountCredentials=$(cat machine_credentials.json | jq .PSKProdServiceAccount)
-op item edit 'aws-dps-2' PSKProdServiceAccount-aws-access-key-id=$(echo $PSKProdServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | tr -d \\n) >/dev/null
-op item edit 'aws-dps-2' PSKProdServiceAccount-aws-secret-access-key=$(echo $PSKProdServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | tr -d \\n) >/dev/null
+PSKProdAccessKey=$(echo $PSKProdServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | tr -d \\n)
+PSKProdSecret=$(echo $PSKProdServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | tr -d \\n)
+
+op item edit 'aws-dps-2' PSKProdServiceAccount-aws-access-key-id=$PSKProdAccessKey --vault empc-lab >/dev/null
+op item edit 'aws-dps-2' PSKProdServiceAccount-aws-secret-access-key=$PSKProdSecret --vault empc-lab >/dev/null
