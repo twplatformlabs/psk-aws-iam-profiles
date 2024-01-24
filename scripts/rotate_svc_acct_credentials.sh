cba6afab61f2aa8fbb1c0521eab33b8f3144535e
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-01-24 12:35:10 -0600

    [nc] non-config changing edits to script
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/rotate_svc_acct_credentials.sh
+++ scripts/rotate_svc_acct_credentials.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+set -eo pipefail
+
 export ENVIRONMENT=$1
-export AWS_ACCOUNT_ID=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_account_id)
-export AWS_ASSUME_ROLE=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_assume_role)
+export AWS_ACCOUNT_ID=$(jq -r .aws_account_id < ${ENVIRONMENT}.auto.tfvars.json)
+export AWS_ASSUME_ROLE=$(jq -r .aws_assume_role < ${ENVIRONMENT}.auto.tfvars.json)
 
 aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ASSUME_ROLE} --role-session-name psk-aws-iam-profiles > credentials
 
-export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
-export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
-export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
-export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
+export AWS_ACCESS_KEY_ID=$(jq -r ".Credentials.AccessKeyId" < credentials)
+export AWS_SECRET_ACCESS_KEY=$(jq -r ".Credentials.SecretAccessKey" < credentials)
+export AWS_SESSION_TOKEN=$(jq -r ".Credentials.SessionToken" < credentials)
+export AWS_DEFAULT_REGION=$(jq -r .aws_region < ${ENVIRONMENT}.auto.tfvars.json)
 
 # Rotate AWS IAM User access credentials. https://pypi.org/project/iam-credential-rotation/
 echo "rotate service account credentials"
@@ -16,7 +18,7 @@ iam-credential-rotation PSKServiceAccounts > machine_credentials.json
 
 # Write new nonprod credentials to 1password
 echo "write PSKNonprodServiceAccount credentials"
-PSKNonprodServiceAccountCredentials=$(cat machine_credentials.json | jq .PSKNonprodServiceAccount)
+PSKNonprodServiceAccountCredentials=$(jq .PSKNonprodServiceAccount < machine_credentials.json)
 PSKNonprodAccessKey=$(echo $PSKNonprodServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | tr -d \\n)
 PSKNonprodSecret=$(echo $PSKNonprodServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | tr -d \\n)
 
@@ -25,7 +27,7 @@ op item edit 'aws-dps-2' PSKNonprodServiceAccount-aws-secret-access-key=$PSKNonp
 
 # Write new prod credentials to 1password
 echo "write PSKProdrodServiceAccount credentials"
-PSKProdServiceAccountCredentials=$(cat machine_credentials.json | jq .PSKProdServiceAccount)
+PSKProdServiceAccountCredentials=$(jq .PSKProdServiceAccount <  machine_credentials.json)
 PSKProdAccessKey=$(echo $PSKProdServiceAccountCredentials | jq .AccessKeyId | sed 's/"//g' | tr -d \\n)
 PSKProdSecret=$(echo $PSKProdServiceAccountCredentials | jq .SecretAccessKey | sed 's/"//g' | tr -d \\n)
 
