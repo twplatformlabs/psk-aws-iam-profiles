afc1130c806259a95955c6aa7df2fb1437c602b3
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-04-06 16:31:30 -0500

    [nc] cleanup bash scriptgs
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/run_awspec_integration_tests.sh
+++ scripts/run_awspec_integration_tests.sh
@@ -2,22 +2,14 @@
 source bash-functions.sh
 
 export ENVIRONMENT=$1
-# export AWS_ACCOUNT_ID=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_account_id)
-# export AWS_ASSUME_ROLE=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_assume_role)
+export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
 
 awsAssumeRole $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_account_id) $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_assume_role)
 
-# aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ASSUME_ROLE} --role-session-name psk-aws-iam-profiles > credentials
-
-# export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
-# export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
-# export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
-export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
-
 # test roles
-rspec test/psk_aws_iam_roles_spec.rb --format documentation
+rspec test/*.rb --format documentation
 
 # if this is the state account then test the profiles
 if [[ ${ENVIRONMENT} == "nonprod" ]]; then
-  rspec test/state-account/psk_aws_iam_profiles_spec.rb --format documentation
+  rspec test/state-account/psk_aws_service_accounts_spec.rb --format documentation
 fi
