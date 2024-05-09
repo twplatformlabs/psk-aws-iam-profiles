682701711c2a82fcd370bd7ae3a713d744006dcf
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-09 16:19:34 -0500

    [nc] clean up comments
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/rotate_svc_acct_credentials.sh
+++ scripts/rotate_svc_acct_credentials.sh
@@ -7,15 +7,6 @@ export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_reg
 
 awsAssumeRole $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_account_id) $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_assume_role)
 
-# export AWS_ACCOUNT_ID=$(jq -r .aws_account_id < ${ENVIRONMENT}.auto.tfvars.json)
-# export AWS_ASSUME_ROLE=$(jq -r .aws_assume_role < ${ENVIRONMENT}.auto.tfvars.json)
-
-# aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ASSUME_ROLE} --role-session-name psk-aws-iam-profiles > credentials
-
-# export AWS_ACCESS_KEY_ID=$(jq -r ".Credentials.AccessKeyId" < credentials)
-# export AWS_SECRET_ACCESS_KEY=$(jq -r ".Credentials.SecretAccessKey" < credentials)
-# export AWS_SESSION_TOKEN=$(jq -r ".Credentials.SessionToken" < credentials)
-
 # Rotate AWS IAM User access credentials. https://pypi.org/project/iam-credential-rotation/
 echo "rotate service account credentials"
 iam-credential-rotation PSKServiceAccounts > machine_credentials.json
