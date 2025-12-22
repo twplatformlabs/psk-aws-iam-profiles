1f9469169a59dd5b71e5e87725eaa0e45be25a18
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-12-21 20:32:32 -0600

    [nc] remove unnecesary test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/state-account/psk_aws_service_accounts_spec.rb
+++ test/state-account/psk_aws_service_accounts_spec.rb
@@ -5,15 +5,7 @@ describe iam_group('PSKNonprodServiceAccountGroup') do
   it { should have_iam_user('PSKNonprodServiceAccount') }
 end
 
-describe iam_policy('PSKNonprodServiceAccountGroup') do
-  it { should exist }
-end
-
 describe iam_group('PSKProdServiceAccountGroup') do
   it { should exist }
   it { should have_iam_user('PSKProdServiceAccount') }
-end
-
-describe iam_policy('PSKProdServiceAccountGroup') do
-  it { should exist }
 end
